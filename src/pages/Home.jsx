bec8af6e6fa39ab101af9501851594913f14b19c
Jenny Huang <jenny.huang@spanflug.de>
2025-11-08 16:25:26 +0100

    fix: adjust Home component layout and update CSS for full height

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className='flex items-center justify-center min-h-screen'>
-      <h1 className='text-3xl font-main font-bold'> COMING SOON </h1>
+    <div className='flex items-center justify-center h-full'>
+      <h1 className='text-3xl font-main font-bold'>COMING SOON</h1>
     </div>
   );
 };
