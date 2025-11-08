5e47b8c23356e829f77b023731cdfe05769514b1
Jenny Huang <jenny.huang@spanflug.de>
2025-11-08 15:54:06 +0100

    feat: replace placeholder div with iframe for SPIN Opening form

--- src/pages/OpeningForm.jsx
+++ src/pages/OpeningForm.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const OpeningForm = () => {
-  return <div>Test</div>;
+  return (
+    <iframe
+      src='https://tally.so/r/D42XDj?transparentBackground=1'
+      width='100%'
+      height='100%'
+      frameBorder='0'
+      style={{
+        border: 0,
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+      }}
+      title='SPIN Opening 2'
+    ></iframe>
+  );
 };
 
 export default OpeningForm;
