9259509fef50ec6ad3bfd41e2678773863231fc7
Jenny Huang <jenny.huang@spanflug.de>
2025-12-06 15:51:25 +0100

    fix: update logo class to enhance visibility with filter effects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -314,7 +314,7 @@ const Home = () => {
         ref={logoRef}
         onLoad={handleImageLoad}
         alt='Spin Logo'
-        className='fixed lg:static left-0 top-0 pointer-events-none select-none object-contain w-full h-auto lg:w-auto lg:h-full mix-blend-difference  bg-white'
+        className='fixed lg:static left-0 top-0 pointer-events-none select-none object-contain w-full h-auto lg:w-auto lg:h-full mix-blend-difference  filter invert brightness-200'
       />
     </div>
   );
