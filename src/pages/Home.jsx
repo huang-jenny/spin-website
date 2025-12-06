cee339ddf1b4706ce9217fac27de124a84db6711
Jenny Huang <jenny.huang@spanflug.de>
2025-12-06 15:50:56 +0100

    feat: add Spotify playlist embed to Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -209,7 +209,18 @@ const Home = () => {
                     encouraged. THE BAR is open Thu–SAT, 7PM until Goldener
                     Reiter Club – located right next door – opens.
                   </div>
-                  <div className='h-[10em]'>Reservierungstool</div>
+                  <div className=''>Reservierungstool</div>
+                  <iframe
+                    data-testid='embed-iframe'
+                    style={{ borderRadius: '12px' }}
+                    src='https://open.spotify.com/embed/playlist/7FenBC9EVkaT3TOeqQzCSm?utm_source=generator&theme=0'
+                    width='100%'
+                    height='352'
+                    frameBorder='0'
+                    allowFullScreen
+                    allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
+                    loading='lazy'
+                  />
                   <div>
                     <p>Cancellations</p>
                     We kindly ask that you inform us as soon as possible if you
