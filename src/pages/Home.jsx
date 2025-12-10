31af1d10580286d583a9530a2bf93ff7585659f0
Jenny Huang <jenny.huang@spanflug.de>
2025-12-10 21:06:02 +0100

    improve responsiveness, little design changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,14 @@ const Home = () => {
   const [pageNumber, setPageNumber] = useState(0);
   const [headerTextIndex, setHeaderTextIndex] = useState(0);
   const headerTexts = ['THU—SAT, 7PM', 'THEKLASTR 1'];
+
+  const headerButtonRightTexts = ['[RESERVATIONS]', '[CLOSE]', '[CLOSE]'];
+  const headerButtonRightLinks = [
+    () => setPageNumber(1),
+    () => setPageNumber(0),
+    () => setPageNumber(0),
+  ];
+
   const [logoHeight, setLogoHeight] = useState(400); // default fallback height
   const breakpointLg = 1024; // Tailwind's lg breakpoint in pixels
 
@@ -105,84 +113,90 @@ const Home = () => {
           className={`h-svh overflow-auto flex flex-col justify-between no-scrollbar`}
         >
           <div
-            className={`lg:mt-0 lg:justify-between flex flex-col lg:h-full p-[4px] lg:p-[8px]`}
+            className={`lg:mt-0 lg:h-full`}
             style={{
               marginTop:
                 window.innerWidth < breakpointLg ? `${logoHeight}px` : '0',
             }}
           >
-            <div>
-              <Header
-                leftText={headerTexts[headerTextIndex]}
-                rightText='[RESERVATIONS]'
-                handleRightTextClick={() => setPageNumber(1)}
-              />
-              <div className='flex flex-col gap-[3em]'>
-                <div>
-                  SPIN is a HiFi bar built around intentional listening and
-                  contemporary bartending. The love for vinyl is what shaped
-                  this place. It is inspired by the culture of listening bars
-                  and high-fidelity sound. The drinks follow the same mindset:
-                  high-quality ingredients, new flavours, crafted with the
-                  precision of modern bartending.
-                </div>
-                <div className='flex flex-col gap-[2em]'>
+            <div className='p-[4px] lg:p-[8px] lg:justify-between flex flex-col min-h-full'>
+              <div>
+                <Header
+                  leftText={headerTexts[headerTextIndex]}
+                  rightText={headerButtonRightTexts[0]}
+                  handleRightTextClick={headerButtonRightLinks[0]}
+                />
+                <div className='flex flex-col gap-[4em]'>
                   <div>
-                    <p>SOUNDSYSTEM</p>
-                    We‘re using equipment chosen to deliver a focused, warm and
-                    immersive listening experience.
+                    SPIN is a Hi-Fi bar built around intentional listening and
+                    contemporary bartending. The love for vinyl is what shaped
+                    this place. It is inspired by the culture of listening bars
+                    and high-fidelity sound. The drinks follow the same mindset:
+                    high-quality ingredients, new flavours, crafted with the
+                    precision of modern bartending.
                   </div>
-                  <div className='flex flex-col gap-[1em]'>
-                    <div className='grid grid-cols-2 2xl:grid-cols-4'>
-                      <div>
-                        <p>Mixer</p>
-                        <p>Turntables</p>
-                        <p>Cartridge</p>
-                      </div>
-                      <div className='col-span-3'>
-                        <p>Resør 3300</p>
-                        <p>Technics 1210 MK7</p>
-                        <p>Taruya 05M-2</p>
-                      </div>
-                      <div className='col-span-4 h-[1em]'></div>
-                      <div>
-                        <p>Speakers</p>
-                        <p>Amplifiers</p>
-                      </div>
+                  <div className='flex flex-col gap-[2em] mb-[2em]'>
+                    <div>
+                      <p>SOUNDSYSTEM</p>
+                      We‘re using equipment chosen to deliver a focused, warm
+                      and immersive listening experience.
+                    </div>
+                    <div className='flex flex-col gap-[1em]'>
+                      <div className='grid grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-4'>
+                        <div className='lg:hidden xl:block'>
+                          <p>Mixer</p>
+                          <p>Turntables</p>
+                          <p>Cartridge</p>
+                        </div>
 
-                      <div className='grid-cols-subgrid grid col-span-1 2xl:col-span-3 gap-[1em]'>
-                        <div>
-                          <p>JBL 4341</p>
-                          <p>Rotel RA-1070</p>
-                          <p>Main Room</p>
+                        <div className='col-span-3'>
+                          <p>Resør 3300</p>
+                          <p>Technics 1210 MK7</p>
+                          <p>Taruya 05M-2</p>
                         </div>
-                        <div>
-                          <p>ATL HD-306</p>
-                          <p>NAD 2140</p>
-                          <p>Bar Area</p>
+
+                        <div className='col-span-4 h-[1em]'></div>
+
+                        <div className='lg:hidden xl:block'>
+                          <p>Speakers</p>
+                          <p>Amplifiers</p>
                         </div>
-                        <div>
-                          <p>Dynaudio MSP 80</p>
-                          <p>Rotel RA-920AX</p>
-                          <p>Back Room</p>
+
+                        <div className='grid-cols-subgrid grid col-span-1 2xl:col-span-3 gap-[1em]'>
+                          <div>
+                            <p>JBL 4341</p>
+                            <p>Rotel RA-1070</p>
+                            <p>Main Room</p>
+                          </div>
+                          <div>
+                            <p>ATL HD-306</p>
+                            <p>NAD 2140</p>
+                            <p>Bar Area</p>
+                          </div>
+                          <div>
+                            <p>Dynaudio MSP 80</p>
+                            <p>Rotel RA-920AX</p>
+                            <p>Back Room</p>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className='flex h-svh lg:h-auto items-end'>
-              <button
-                onClick={() => setPageNumber(2)}
-                className='hover:text-hover-color'
-              >
-                [IMPRINT]
-              </button>
+              <div className='flex h-svh lg:h-auto items-end'>
+                <button
+                  onClick={() => setPageNumber(2)}
+                  // className='hover:text-hover-color'
+                >
+                  [IMPRINT]
+                </button>
+              </div>
             </div>
           </div>
         </div>
 
+        <div className='h-[400px]'></div>
         {/* Imprint Page */}
 
         <div
@@ -199,8 +213,8 @@ const Home = () => {
               }}
             >
               <Header
-                rightText='[CLOSE]'
-                handleRightTextClick={() => setPageNumber(0)}
+                rightText={headerButtonRightTexts[1]}
+                handleRightTextClick={headerButtonRightLinks[1]}
               />
               <div className='flex flex-col gap-[3em]'>
                 <div className='flex flex-col gap-[2em]'>
@@ -229,8 +243,8 @@ const Home = () => {
               }}
             >
               <Header
-                rightText='[CLOSE]'
-                handleRightTextClick={() => setPageNumber(0)}
+                rightText={headerButtonRightTexts[2]}
+                handleRightTextClick={headerButtonRightLinks[2]}
               />
               <div className='mt-4 flex flex-col gap-[2em]'>
                 <section
@@ -255,21 +269,20 @@ const Home = () => {
                   </div>
                   <div>
                     <p>Kontakt:</p>
-                    <a
-                      href='mailto:contact@spin-bar.de'
-                      className='hover:underline'
-                    >
-                      contact@spin-bar.de
-                    </a>
+                    <a href='mailto:contact@spin-bar.de'>contact@spin-bar.de</a>
                   </div>
                   <div>
                     <p>USt-Id-Nr:</p> DE295571897
                   </div>
                   <div>
-                    <p>CODE:</p>JENNY HUANG
+                    <p>CODE:</p>
+                    <a href='https://www.instagram.com/jeyrototo/'>
+                      JENNY HUANG
+                    </a>
                   </div>
                   <div>
-                    <p>DESIGN:</p>CUSTOM TIP
+                    <p>DESIGN:</p>
+                    <a href='https://custom-tip.com/'>CUSTOM TIP</a>
                   </div>
                 </section>
 
@@ -288,35 +301,60 @@ const Home = () => {
                   <h2 id='privacy'>Datenschutzerklärung</h2>
 
                   <h3 className='mt-4'>1. Datenschutz auf einen Blick</h3>
-                  <p>
+                  <div>
                     <p>Allgemeine Hinweise</p>
-                  </p>
-                  <p>...</p>
+                  </div>
+                  <div>...</div>
                 </section>
               </div>
             </div>
           )}
         </div>
       </div>
-      <img
-        src={spinlogo}
+      <div
+        className='w-full h-auto lg:w-auto lg:h-full  fixed lg:static left-0 top-0'
         ref={logoRef}
-        onLoad={handleImageLoad}
-        alt='Spin Logo'
-        className='fixed lg:static left-0 top-0 pointer-events-none select-none object-contain w-full h-auto lg:w-auto lg:h-full '
-      />
+      >
+        <img
+          src={spinlogo}
+          onLoad={handleImageLoad}
+          alt='Spin Logo'
+          className=' object-contain w-full h-auto lg:w-auto lg:h-full  pointer-events-none select-none'
+        />
+        <HeaderMobile
+          leftText={pageNumber === 0 ? headerTexts[headerTextIndex] : ''}
+          rightText={headerButtonRightTexts[pageNumber]}
+          handleRightTextClick={headerButtonRightLinks[pageNumber]}
+        />
+      </div>
     </div>
   );
 };
 
 const Header = ({ leftText, rightText, handleRightTextClick }) => {
   return (
-    <div className='flex flex-row w-full mb-[18px]'>
+    <div className='flex-col xl:flex-row w-full mb-[18px] hidden lg:flex'>
       <div className='flex-1'>{leftText}</div>
       <div className='flex-1'>
         <button
           onClick={handleRightTextClick}
-          className='hover:text-hover-color'
+          // className='hover:text-hover-color'
+        >
+          {rightText}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const HeaderMobile = ({ leftText, rightText, handleRightTextClick }) => {
+  return (
+    <div className='flex flex-row w-full mb-[18px] p-[4px] lg:p-[8px] lg:hidden'>
+      <div className='flex-1'>{leftText}</div>
+      <div className='flex-1'>
+        <button
+          onClick={handleRightTextClick}
+          // className='hover:text-hover-color'
         >
           {rightText}
         </button>
